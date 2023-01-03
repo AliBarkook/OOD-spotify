0203abcf5580ad7c06831cabd6bba7a69bddf4ee
a.barkook <alibarkook@gmail.com>
2023-01-03 08:44:02 +0330

    init dotnet 6 comsole project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOD_spotify.src.classes;
+
+Console.WriteLine("Hello, World!");
+
+
+var x = new song(3.14, false, 0, new artist("mehrad hidden"));
+x.play();
+Console.WriteLine(x);
